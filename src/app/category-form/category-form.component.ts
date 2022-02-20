f3c75bf617f305c832bd81e00f97d64c19b48133
Sanchit Batra <sanchitbatra@Sanchits-MacBook-Air.local>
2022-02-21 03:37:41 +0530

    Started with the category add form

--- src/app/category-form/category-form.component.ts
+++ src/app/category-form/category-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-category-form',
@@ -7,9 +8,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryFormComponent implements OnInit {
 
+  categoryForm : FormGroup;
+  imgSrc:string;
+  selectedImage : any = null;
+
   constructor() { }
 
   ngOnInit(): void {
+
+    this.categoryForm = new FormGroup({
+      'categoryName' : new FormControl(''), 
+      'imageUrl' : new FormControl('')
+    });
+
+    this.imgSrc = "../../assets/default.png";
+  }
+
+  showPreview(event : any)
+  {
+    if(event.target.files && event.target.files[0])
+    {
+      const reader = new FileReader();
+      reader.onload = (e:any)=>{
+        this.imgSrc = e.target.result;
+      }
+      reader.readAsDataURL(event.target.files[0]);
+      this.selectedImage = event.target.files[0];
+    }
+    //if someone clicks cancel
+    else {
+      this.imgSrc = "../../assets/default.png";
+      this.selectedImage = null;
+    }
   }
 
 }
