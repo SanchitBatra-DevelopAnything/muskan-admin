bdf5bf3da78dd6f07677d518206767a6423fd6f1
Sanchit Batra <sanchitbatra@Sanchits-MacBook-Air.local>
2022-02-27 01:48:27 +0530

    Completing notification delete events

--- src/app/notifications/notifications.component.ts
+++ src/app/notifications/notifications.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { ApiserviceService } from '../services/apiservice.service';
+import { UtilityServiceService } from '../services/utility-service.service';
 
 @Component({
   selector: 'app-notifications',
@@ -11,18 +13,24 @@ export class NotificationsComponent implements OnInit {
   retailerNotifications : any;
   notificationKeys : any[];
   isLoading:boolean;
+  retailerNotificationDeletedSub : Subscription;
 
-  constructor(private apiService : ApiserviceService) { }
+  constructor(private apiService : ApiserviceService , private utilityService : UtilityServiceService) { }
 
   ngOnInit(): void {
     this.isLoading = true;
+    this.retailerNotificationDeletedSub = this.utilityService.retailerNotificationDeleted.subscribe((_)=>{
+      this.getRetailerNotifications();
+    });
+    this.getRetailerNotifications();
+  }
+
+  getRetailerNotifications()
+  {
     this.apiService.getRetailerNotifications().subscribe((notis)=>{
-      // console.log("Noti " , notis);
       this.retailerNotifications = Object.values(notis);
       this.notificationKeys = Object.keys(notis);
       this.isLoading = false;
-      // console.log("Data" , this.retailerNotifications);
-      // console.log("keys" , this.notificationKeys);
     });
   }
 
