dd7160e4c031cb3c906afd82b341243d2dd879df
Sanchit Batra <sanchitbatra@Sanchits-MacBook-Air.local>
2022-03-21 14:58:24 +0530

    Passed item inft from list to item

--- src/app/item/item.component.ts
+++ src/app/item/item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-item',
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ItemComponent implements OnInit {
 
+  @Input()
+  item : any;
+
+  @Input()
+  itemKeyInDb: string;
+
   constructor() { }
 
   ngOnInit(): void {
+    console.log("Forming item with");
+    console.log(this.item.itemName , this.item.offer + "%" , this.itemKeyInDb)
   }
 
 }
