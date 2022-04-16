caadbdc16b38444ef33d21a3bbf32b5e9b269518
Sanchit Batra <sanchitbatra@Sanchits-MacBook-Air.local>
2022-04-17 04:19:43 +0530

    Adding chef notifications server key

--- src/app/services/utility-service.service.ts
+++ src/app/services/utility-service.service.ts
@@ -13,6 +13,8 @@ export class UtilityServiceService {
   itemDeleted:Subject<string>;
   itemUpdated:Subject<string>;
 
+  chefNotificationServerKey:string;
+
   constructor() { 
     this.loggedInStatusUpdated = new Subject<boolean>();
     this.categoryDeleted = new Subject<string>();
@@ -20,6 +22,8 @@ export class UtilityServiceService {
     this.refreshRetailerNotificationCount = new Subject<string>();
     this.itemDeleted = new Subject<string>();
     this.itemUpdated = new Subject<string>();
+
+    this.chefNotificationServerKey = "AAAA1CskWfc:APA91bELCsy-GM2n8hPD3Sc4vaanR3ymcIP8mttKC5rhfg9nU4eDVcxcKOxOICxx5B6zdnGce4bBvPfzOB_NzMUg3iT_hHRQbhIyfTAduQxkkVOYTx0hQd0S1GJaPbmtmJrIKdb4_X1f";
   }
 
 }
