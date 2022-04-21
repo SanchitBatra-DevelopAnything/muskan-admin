462047e93168216e55f0d4492bd617b709844c89
Sanchit Batra <sanchitbatra@Sanchits-MacBook-Air.local>
2022-04-21 14:15:01 +0530

    Adding UI to display flavours

--- src/app/flavours-screen/flavours-screen.component.ts
+++ src/app/flavours-screen/flavours-screen.component.ts
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FlavoursScreenComponent implements OnInit {
 
+  flavours : {flavourName : string , shopPrice : number , customerPrice : number}[];
+
   constructor() { }
 
   ngOnInit(): void {
+    this.flavours = [{flavourName : "pineapple" , shopPrice : 250 , customerPrice : 340} ,{flavourName : "chocloate" , shopPrice : 250 , customerPrice : 340} ];
+  }
+
+  hello()
+  {
+    console.log("hekkli");
   }
 
 }
